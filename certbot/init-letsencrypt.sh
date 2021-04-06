44b0b210171ebef2a7522b306da49d84c7bf1608
milat <a2milat.diaz@gmail.com>
2021-04-06 20:28:17 -0300

    removing www domain from certbot

--- certbot/init-letsencrypt.sh
+++ certbot/init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(sigga.milat.dev www.sigga.milat.dev)
+domains=(sigga.milat.dev)
 rsa_key_size=4096
 data_path="/var/www/sigga-infra/certbot"
 email="antonio@milat.dev" # Adding a valid address is strongly recommended
